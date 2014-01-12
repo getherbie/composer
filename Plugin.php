0e1f0cb74a1d6b1248b7e42e910832a6d1c5f16f
Thomas Breuss <thomasbreuss@gmx.ch>
2014-01-12 07:29:17 +0100

    Added composer plugin

---
+++ Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace herbie\composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new TemplateInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
