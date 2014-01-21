815e7e2f536e8c8e949bf597f1d9bc0c6bf993b8
Thomas Breuss <>
2014-01-21 07:35:28 +0100

    Added licence information

--- Installer.php
+++ Installer.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace herbie\composer;
 
 use Composer\Package\PackageInterface;
@@ -47,5 +56,5 @@ class Installer extends LibraryInstaller
 			}
 		}
 	}
-    
+
 }
